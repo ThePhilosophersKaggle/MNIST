653bcd01b5cacb75117827307b2d37fd44b8763e
fanch44 <fanchmahe.asus@gmail.com>
2018-06-06 19:57:39 +0200

    Ajout de la sauvegarde

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from models import CNN
+from models import CNN, save_model, load_model
 from preprocessing import preprocess
 
 
 X0, y0, Y0, X1, y1, Y1 = preprocess()
-CNN(X0, Y0, X1, Y1, fit=True)
+model = CNN(X0, Y0, X1, Y1, fit=True)
+
+save_model(model)
--- models.py
+++ models.py
@@ -1,5 +1,37 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Conv2D, BatchNormalization, MaxPooling2D, Dropout
+from keras.models import model_from_json
+
+
+def save_model(model, name='model'):
+    """
+    Sauvegarde le modèle dans model.json et les poids dans model.h5
+    :param model: modèle Keras
+    :param name: nom des fichiers de sauvegarde
+    """
+    model_json = model.to_json()
+    with open(name + '.json', 'w') as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(name + '.h5')
+    print("Saved model to disk")
+ 
+
+def load_model(name='model'):
+    """
+    Charge le modèle et les poids associés
+    :param name: nom des fichiers de sauvegarde
+    :return: loaded_model
+    """
+    # load json and create model
+    json_file = open(name + '.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(name + '.h5')
+    print("Loaded model from disk")
+    return loaded_model
 
 
 def CNN(X0, Y0, X1, Y1, fit=False):
